ff99086e716478e9acc1c1b556de1e6efad7c736
Alex_djangp_projects <aleksone_01@mail.ru>
2021-09-19 15:21:24 +0300

    add new pages

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import WishList
+from .forms import ProductForm
+from datetime import datetime
 
 def index(request):
     return render(request, 'index.html', {})
@@ -9,11 +11,22 @@ def about(request):
 
 def list_page(request, pk):
     wishlist = get_object_or_404(WishList, pk=pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        instance_product=form.save()
+        product = form.save()
+        wishlist.product.add(product)
+        wishlist.save()
+    else:
+        form = ProductForm()
+
     return render(
         request,
         'wish_list.html',
             {
                 'wishlist': wishlist,
-                'is_owner_list':wishlist.owner == request.user
+                'is_owner_list': wishlist.owner == request.user,
+                'form': form
             }
         )
